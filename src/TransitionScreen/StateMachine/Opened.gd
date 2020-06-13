10b6e3fc59b03db876632759166621f578ab655a
Artyom Volkov <tricky.fat.cat@gmail.com>
2020-06-14 02:28:14 +0300

    Pendulum and egg fixes
    1. Decrease egg collision size;
    2. Deleted debug functionality from transition screen;
    3. Added recalculations for debug lines of pendulum;

--- src/TransitionScreen/StateMachine/Opened.gd
+++ src/TransitionScreen/StateMachine/Opened.gd
@@ -2,8 +2,6 @@ extends State
 
 
 func unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_accept"):
-		TransitionScreen.start_transition()
 	return
 
 
--- src/Traps/Pendulum/Pendulum.gd
+++ src/Traps/Pendulum/Pendulum.gd
@@ -7,14 +7,13 @@ export(float) var fluctuation_duration: = 1.0
 export(float) var pause_duration: = 0.25
 export(float) var angle_min_default: = 45.0
 export(float) var angle_max_default: = -45.0
-export(bool) var is_debug_enabled: = false
 
 onready var tween: Tween = $Tween
 onready var pauseTimer: Timer = $PauseTimer
 onready var rotation_direction: int = 1 if is_clockwise else -1
 onready var tween_type = Tween.TRANS_LINEAR if is_rotation_endless else Tween.TRANS_QUINT
-onready var angle_min = angle_min_default if !is_rotation_endless else 0
-onready var angle_max = angle_max_default if !is_rotation_endless else 360
+onready var angle_min: float = angle_min_default if !is_rotation_endless else 0
+onready var angle_max: float = angle_max_default if !is_rotation_endless else 360
 
 
 func _on_Tween_tween_all_completed() -> void:
@@ -30,17 +29,31 @@ func _on_PauseTimer_timeout() -> void:
 
 
 func _ready() -> void:
-	angle_min *= rotation_direction
-	angle_max *= rotation_direction
+	if is_rotation_endless:
+		angle_min *= rotation_direction
+		angle_max *= rotation_direction
+	
 	rotation_degrees = angle_min
 	pauseTimer.wait_time = pause_duration
 	tween.repeat = is_rotation_endless
 	start_tween(angle_min, angle_max)
-	switch_debug_mode()
+	
+	if !Engine.editor_hint:
+		disable_lines()
 
 
 func _process(delta: float) -> void:
-	if is_debug_enabled:
+	if Engine.editor_hint:
+		angle_min = angle_min_default if !is_rotation_endless else 0
+		angle_max = angle_max_default if !is_rotation_endless else 360
+		rotation_direction = 1 if is_clockwise else -1
+		
+		if is_rotation_endless:
+			angle_min *= rotation_direction
+			angle_max *= rotation_direction
+		
+		pauseTimer.wait_time = pause_duration
+		tween.repeat = is_rotation_endless
 		$MinAngle.rotation_degrees = angle_min - rotation_degrees
 		$MaxAngle.rotation_degrees = angle_max - rotation_degrees
 
@@ -65,6 +78,6 @@ func restart_tween() -> void:
 		start_tween(angle_max, angle_min)
 
 
-func switch_debug_mode() -> void:
-	$MinAngle.visible = is_debug_enabled
-	$MaxAngle.visible = is_debug_enabled
+func disable_lines() -> void:
+	$MinAngle.visible = false
+	$MaxAngle.visible = false
