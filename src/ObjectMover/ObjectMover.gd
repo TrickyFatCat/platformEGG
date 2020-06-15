9953a92e2129408e2fd74ecab48df20b085d0e3d
Artyom Volkov <tricky.fat.cat@gmail.com>
2020-06-16 01:01:19 +0300

    Object mover, moving saw

--- src/ObjectMover/ObjectMover.gd
+++ src/ObjectMover/ObjectMover.gd
@@ -1,7 +1,7 @@
 tool
 extends Node
 
-export(NodePath) var target_object: = ""
+export(NodePath) var target_object
 export(float) var travel_duration: = 1.0
 export(float) var pause_duration: = 1.0
 export(bool) var is_cycled: = false
@@ -14,6 +14,7 @@ var target_position: Vector2
 onready var points: Array = $TargetPoints.get_children()
 onready var tween: Tween = $Tween
 onready var pauseTimer: Timer = $PauseTimer
+onready var target_node: = get_node(target_object)
 
 
 func _on_Tween_tween_all_completed() -> void:
@@ -25,27 +26,48 @@ func _on_Tween_tween_all_completed() -> void:
 
 func _on_PauseTimer_timeout() -> void:
 	start_movement()
-	pass # Replace with function body.
 
 
 func _ready() -> void:
-	pauseTimer.wait_time = pause_duration
+	if pause_duration > 0:
+		pauseTimer.wait_time = pause_duration
 	
 	if is_active:
 		start_movement()
 
 
+func _process(delta: float) -> void:
+	for point in points:
+		$Altitude.points.append(point.position)
+
+
 func start_movement() -> void:
-	pass
+	calculate_target_position()
+	
+	if point_index != points.size():
+		start_tween()
+		point_index += 1
+
+
+func start_tween() -> void:
+	var tween_type: = Tween.TRANS_LINEAR if pause_duration == 0 else Tween.TRANS_QUINT
+	tween.interpolate_property(
+		target_node,
+		"global_position",
+		target_node.global_position,
+		target_position,
+		travel_duration,
+		tween_type,
+		Tween.EASE_IN_OUT
+	)
+	tween.start()
 
 
 func calculate_target_position() -> void:
 	if point_index == points.size() and !is_oneshot:
-		if is_cycled:
+		if !is_cycled:
 			points.invert()
 		
 		point_index = 0 if is_cycled else 1
 	
 	target_position = points[point_index].global_position
-
-
