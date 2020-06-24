3203cd0e9eaac3b18eb84b59686843e08aab41b2
Artyom Volkov <tricky.fat.cat@gmail.com>
2020-06-24 22:36:25 +0300

    Updated camera shaker

---
+++ src/GameCamera/CameraShaker/CameraShaker.gd
@@ -0,0 +1,70 @@
+extends Node
+
+const TRANS = Tween.TRANS_SINE
+const EASE = Tween.EASE_IN_OUT
+
+var amplitude : int = 0
+var priority : int = 0
+
+onready var camera : Camera2D = get_parent()
+onready var frequencyTimer : Timer = $FrequencyTimer
+onready var durationTimer : Timer = $DurationTimer
+onready var shakeTween : Tween = $ShakeTween
+
+
+func _on_FrequencyTimer_timeout() -> void:
+	_new_shake()
+
+
+func _on_DurationTimer_timeout() -> void:
+	_reset()
+	frequencyTimer.stop()
+
+
+func start(
+	duration: float = 0.2,
+	frequency: float = 15.0,
+	amplitude: int = 16,
+	priority: int = 0
+	) -> void:
+	if priority >= self.priority:
+		self.amplitude = amplitude
+		durationTimer.wait_time = duration
+		frequencyTimer.wait_time = 1 / frequency
+		durationTimer.start()
+		frequencyTimer.start()
+		_new_shake()
+
+
+func _new_shake() -> void:
+	var rand = Vector2.ZERO
+	rand.x = rand_range(-amplitude, amplitude)
+	rand.y = rand_range(-amplitude, amplitude)
+# warning-ignore:return_value_discarded
+	shakeTween.interpolate_property(
+		camera,
+		"offset",
+		camera.offset,
+		rand,
+		frequencyTimer.wait_time,
+		TRANS,
+		EASE
+	)
+# warning-ignore:return_value_discarded
+	shakeTween.start()
+
+
+func _reset() -> void:
+# warning-ignore:return_value_discarded
+	shakeTween.interpolate_property(
+		camera, "offset",
+		camera.offset,
+		Vector2.ZERO,
+		frequencyTimer.wait_time,
+		TRANS,
+		EASE
+	)
+# warning-ignore:return_value_discarded
+	shakeTween.start()
+	priority = 0
+
---
+++ src/GameCamera/GameCamera.gd
@@ -0,0 +1,17 @@
+extends Camera2D
+
+onready var cameraShaker : Node = $CameraShaker
+
+
+func _ready() -> void:
+# warning-ignore:return_value_discarded
+	Events.connect("player_took_damage", self, "activate_player_shake")
+# warning-ignore:return_value_discarded
+	Events.connect("egg_took_damage", self, "activate_egg_shake")
+
+
+func activate_player_shake() -> void:
+	cameraShaker.start(0.2, 50, 1)
+
+func activate_egg_shake() -> void:
+	cameraShaker.start(0.2, 50, 2, 1)
