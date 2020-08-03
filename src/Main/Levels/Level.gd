208f4e9f8401eebe82960d5e9d32f259b97be8d4
Artyom Volkov <tricky.fat.cat@gmail.com>
2020-08-03 21:37:46 +0300

    Level loader update

--- src/Main/Levels/Level.gd
+++ src/Main/Levels/Level.gd
@@ -1,16 +1,23 @@
 extends Node
 class_name GameLevel
 
+export(bool) var load_custom_level: = false
 export(String, FILE, "*.tscn") var next_level: = ""
 
 
 func _get_configuration_warning() -> String:
 	var warning: String = "Next level must be chosen."
-	return warning if !next_level else ""
+	return warning if !next_level and load_custom_level else ""
 
 
 func _ready() -> void:
 	if !Engine.editor_hint:
 		Events.emit_signal("level_loaded")
-		LevelLoader.next_level = next_level
-		LevelLoader.current_level = self.name
+
+		if load_custom_level:
+			LevelLoader.next_level = next_level
+			LevelLoader.current_level = self.name
+		else:
+			LevelLoader.next_level_id = LevelLoader.current_level_id + 1
+			LevelLoader.current_level_id = LevelLoader.next_level_id
+			LevelLoader.next_level = LevelLoader.levels[LevelLoader.next_level_id]
--- src/Singletons/LevelLoader.gd
+++ src/Singletons/LevelLoader.gd
@@ -1,8 +1,44 @@
 extends Node
 
-var first_level: String = "res://levels/Level_Test.tscn"
+var first_level_id: int = 0
 var next_level: String
 var current_level: String
+var next_level_id: int
+var current_level_id: int = 0
+var levels: PoolStringArray = [
+	"res://levels/GameLevels/World01/W01L01.tscn",
+	"res://levels/GameLevels/World01/W01L02.tscn",
+	"res://levels/GameLevels/World01/W01L03.tscn",
+	"res://levels/GameLevels/World01/W01L04.tscn",
+	"res://levels/GameLevels/World01/W01L05.tscn",
+	"res://levels/GameLevels/World01/W01L06.tscn",
+	"res://levels/GameLevels/World01/W01L07.tscn",
+	"res://levels/GameLevels/World01/W01L08.tscn",
+	"res://levels/GameLevels/World01/W01L09.tscn",
+	"res://levels/GameLevels/World01/W01L10.tscn",
+	"res://levels/GameLevels/World01/W01L11.tscn",
+	"res://levels/GameLevels/World01/W01L12.tscn",
+	"res://levels/GameLevels/World01/W01L13.tscn",
+	"res://levels/GameLevels/World01/W01L14.tscn",
+	"res://levels/GameLevels/World01/W01L15.tscn",
+	"res://levels/GameLevels/World01/W01L16.tscn",
+	"res://levels/GameLevels/World01/W01L17.tscn",
+	"res://levels/GameLevels/World01/W01L18.tscn",
+	"res://levels/GameLevels/World01/W01L19.tscn",
+	"res://levels/GameLevels/World01/W01L20.tscn",
+	"res://levels/GameLevels/World01/W01L21.tscn",
+	"res://levels/GameLevels/World01/W01L22.tscn",
+	"res://levels/GameLevels/World01/W01L23.tscn",
+	"res://levels/GameLevels/World01/W01L24.tscn",
+	"res://levels/GameLevels/World01/W01L25.tscn",
+	"res://levels/GameLevels/World01/W01L26.tscn",
+	"res://levels/GameLevels/World01/W01L27.tscn",
+	"res://levels/GameLevels/World01/W01L28.tscn",
+	"res://levels/GameLevels/World01/W01L29.tscn",
+	"res://levels/GameLevels/World01/W01L30.tscn",
+	"res://levels/GameLevels/World01/W01L31.tscn",
+	"res://levels/GameLevels/World01/W01L32.tscn"
+]
 
 
 func _ready() -> void:
