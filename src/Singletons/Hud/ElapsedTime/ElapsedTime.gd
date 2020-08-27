ee1b2a5d60a3c1ed6df1d81d0f5fb0076f9a5e1f
Artyom Volkov <tricky.fat.cat@gmail.com>
2020-08-27 10:40:43 +0300

    Level timer fix

--- src/Singletons/Hud/ElapsedTime/ElapsedTime.gd
+++ src/Singletons/Hud/ElapsedTime/ElapsedTime.gd
@@ -1,12 +1,19 @@
 extends Label
 
+const TIME : String = "%02d:%02d.%03d"
+
+
+
+
 func _process(delta: float) -> void:
-	if not LevelLoader.is_level_timer_active:
-		text = "00:00.000"
-		return
-		
 	var time = LevelLoader.level_time
 	var minutes = time / 60
 	var seconds = int(time) % 60
 	var miliseconds = int(fmod(time, seconds) * 1000)
-	text = "%02d:%02d.%03d" % [minutes, seconds, miliseconds]
+
+	if not LevelLoader.is_level_timer_active or miliseconds < 0:
+		minutes = 0
+		seconds = 0
+		miliseconds = 0
+
+	text = TIME % [minutes, seconds, miliseconds]
