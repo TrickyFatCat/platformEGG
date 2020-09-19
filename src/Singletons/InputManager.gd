376bf093cd05346e026fd1b13952bddb3d0ee366
Artyom Volkov <tricky.fat.cat@gmail.com>
2020-09-19 14:00:51 +0300

    Input manager slight update

--- src/Singletons/InputManager.gd
+++ src/Singletons/InputManager.gd
@@ -2,7 +2,8 @@ extends Node
 
 enum input_device {
 	KEYBOARD,
-	GAMEPAD
+	PS4,
+	XBOX
 }
 
 const JOY_DEADZONE : float = 0.25
@@ -13,13 +14,21 @@ var joy_id_current : int = JOY_ID_DEFAULT
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventKey or event is InputEventMouse:
+	if event is InputEventKey:
 		current_input_device = input_device.KEYBOARD
 		Events.emit_signal("input_device_changed", current_input_device)
 		return
 
-	if event is InputEventJoypadButton or event is InputEventJoypadMotion:
+	if event is InputEventJoypadButton:
 		joy_id_current = event.device
-		current_input_device = input_device.GAMEPAD
+
+		match Input.get_joy_name(joy_id_current):
+			"PS4 Controller":
+				current_input_device = input_device.PS4
+				pass
+			"XInput Gamepad":
+				current_input_device = input_device.XBOX
+				pass
+			
 		Events.emit_signal("input_device_changed", current_input_device)
 		return
