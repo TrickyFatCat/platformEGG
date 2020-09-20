1fd44bd9d2eb4b7fb97b5eef3c85850c2c1b7dcb
Tricky Fat Cat <tricky.fat.cat@gmail.com>
2020-09-20 17:17:14 +0300

    Player and finish point fix

--- src/FinishPoint/FinishPoint.gd
+++ src/FinishPoint/FinishPoint.gd
@@ -9,7 +9,6 @@ func _on_body_entered(body: PhysicsBody2D) -> void:
 	is_body_egg(body)
 	
 	if is_player_inside and is_egg_inside:
-		# TODO add result window call here
 		Events.emit_signal("open_finish_screen")
 		Global.call_deferred("deactivate_player")
 
@@ -22,7 +21,9 @@ func _on_body_exited(body: PhysicsBody2D) -> void:
 func is_body_player(body: PhysicsBody2D) -> void:
 	if body is Player:
 		is_player_inside = !is_player_inside
-		is_egg_inside = Global.player.is_with_egg
+
+		if Global.player.is_with_egg:
+			is_egg_inside = Global.player.is_with_egg
 
 
 func is_body_egg(body: PhysicsBody2D) -> void:
