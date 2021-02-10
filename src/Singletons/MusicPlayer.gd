a9a12452ab41571db6f1b6b83ed191fa300b757c
Tricky Fat Cat <tricky.fat.cat@gmail.com>
2021-02-10 21:01:38 +0300

    Simplify music player logic

--- src/Singletons/MusicPlayer.gd
+++ src/Singletons/MusicPlayer.gd
@@ -1,87 +1,57 @@
 extends AudioStreamPlayer
 
-enum mode {
-	TRACK,
-	SET
-}
-
 const MIN_VOLUME : float = -80.0
-const FADE_TIME : float = 0.75
+const FADE_TIME : float = 0.25
 
-var music
-var _current_mode
-var _tracks
 var _fade_tween : Tween
-var volume_factor : float = 0.0
+var volume_factor : float = 0.0 setget set_volume
 
-onready var default_volume : float = volume_db
-onready var current_volume : float = MIN_VOLUME
+var _default_volume : float
+
 
 func _init() -> void:
-	connect("finished", self, "_on_track_finished")
 	_fade_tween = Tween.new()
 	self.add_child(_fade_tween)
 	_fade_tween.connect("tween_all_completed", self, "_on_fade_ended")
+	_default_volume = volume_db
+	set_volume(0.0)
 
-
-func _ready() -> void:
-	_set_volume(0.0)
-	TransitionScreen.connect("screen_opened", self, "_start_playing_music")
-	TransitionScreen.connect("screen_closed", self, "_stop_playing_music")
-
-func _on_track_finished() -> void:
-	match _current_mode:
-		mode.TRACK:
-			print_debug("Finished")
-			pass
-		mode.SET:
-			pass
 
 func _on_fade_ended() -> void:
 	if playing and volume_factor == 0.0:
 		playing = false
 
-func play_music_track(track : AudioStream) -> void:
-	_current_mode = mode.TRACK
-	pass
 
-
-func play_music_set(tracks) -> void:
-	_current_mode = mode.SET
-	self._tracks = tracks
-	pass
-
-
-func _start_playing_music() -> void:
+func play_track(track : AudioStream, fade_time : float = FADE_TIME) -> void:
+	if playing:
+		playing = false
+	
+	print_debug(fade_time)
+	stream = track
 	playing = true
-	_fade_in()
+	_fade_in(fade_time)
 
 
-func _stop_playing_music() -> void:
-	_fade_out()
+func set_volume(value: float) -> void:
+	volume_factor = value
+	volume_db = lerp(MIN_VOLUME, _default_volume, volume_factor)
 
 
-func _set_volume(volume_factor: float) -> void:
-	self.volume_factor = volume_factor
-	current_volume = lerp(MIN_VOLUME, default_volume, volume_factor)
-	volume_db = current_volume
+func _fade_in(fade_time: float = FADE_TIME) -> void:
+	_start_tween(0.0, 1.0, fade_time)
 
 
-func _fade_in() -> void:
-	_start_tween(0.0, 1.0)
+func _fade_out(fade_time: float = FADE_TIME) -> void:
+	_start_tween(1.0, 0.0, fade_time)
 
 
-func _fade_out() -> void:
-	_start_tween(1.0, 0.0)
-
-
-func _start_tween(initial_value: float, target_value: float) -> void:
+func _start_tween(initial_value: float, target_value: float, fade_time : float) -> void:
 	_fade_tween.interpolate_method(
 		self,
-		"_set_volume",
+		"set_volume",
 		initial_value,
 		target_value,
-		FADE_TIME,
+		fade_time,
 		Tween.TRANS_LINEAR,
 		Tween.EASE_IN
 	)
