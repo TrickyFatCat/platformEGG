900dcd95fbc652e6968d120332253a18400b9993
Tricky Fat Cat <tricky.fat.cat@gmail.com>
2021-02-21 13:11:15 +0300

    Add steps sound playing logic

--- src/Player/StateMachine/Run.gd
+++ src/Player/StateMachine/Run.gd
@@ -1,9 +1,16 @@
 extends State
 
+const MOVE_SOUND_DELAY : float = 0.2
+
+var step_sound : String = "res://sounds/sfx_goose.wav"
+
 onready var player: Player = Global.player
 onready var move: State = get_parent()
 onready var sprite: AnimatedSprite = player.get_node("Sprite")
+onready var move_timer: Timer = $MoveTimer
 
+func _ready() -> void:
+	move_timer.wait_time = MOVE_SOUND_DELAY
 
 func unhandled_input(event: InputEvent) -> void:
 	move.unhandled_input(event)
@@ -21,7 +28,16 @@ func physics_process(delta: float) -> void:
 func enter(msg: Dictionary = {}) -> void:
 	move.enter()
 	sprite.play("run")
-
+	_play_step_sound()
+	move_timer.connect("timeout", self, "_play_step_sound")
 
 func exit() -> void:
 	move.exit()
+	move_timer.disconnect("timeout", self, "_play_step_sound")
+
+
+
+func _play_step_sound() -> void:
+	AudioPlayer.play(step_sound)
+	move_timer.start()
+	pass
